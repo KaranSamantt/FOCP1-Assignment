0968acf0812918d006b666976592bb9040ae100a
KaranSamantt <karan.samant13@gmail.com>
2025-10-31 15:00:20 +0530

    Add files via upload

---
+++ q7.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int rows = 5;
+
+    for(int i=1;i<=rows;i++){
+        for(int j=1; j<=i;j++){
+            printf("%d", (j-1)%2);
+        }
+
+        for(int s = 1; s <= (rows - i) * 4; s++) {
+            printf(" ");
+        }
+
+        for(int j=1; j<=i;j++){
+            printf("%d", (j-1)%2);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
